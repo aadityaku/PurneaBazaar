a993a24c45851736be708ed8a20beb3ff8b774db
Aaditya kumar <aadityakumar368a@gmail.com>
2022-05-05 17:12:58 +0530

    first commit

---
+++ database/migrations/2022_05_05_084502_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string("title");
+            $table->foreignId("category_id")->constrained();
+            $table->foreignId("brand_id")->constrained();
+            $table->string("image");
+            $table->string("price");
+            $table->string("discount_price");
+            $table->string("stock");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
